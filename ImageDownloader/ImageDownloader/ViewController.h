22a5255db92a8a6797addad517670974ea5c282e
Vu Khoi Nguyen <nguyen@misfit.com>
2017-04-28 20:35:43 +0700

    Init Project ImageDownloader.

---
+++ ImageDownloader/ImageDownloader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageDownloader
+//
+//  Created by Nguyen Vu on 4/28/17.
+//  Copyright © 2017 Nguyen Vu Khoi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageDownloader/ImageDownloader/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ImageDownloader
+//
+//  Created by Nguyen Vu on 4/28/17.
+//  Copyright © 2017 Nguyen Vu Khoi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
